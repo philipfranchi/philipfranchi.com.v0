e4bb3735a1b40ddadde01de71f901351e24677c9
Philip <pfranchi@penguinrandomhouse.com>
2017-04-04 15:56:14 -0400

    Added resume button

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-var express = require('express')
-var app = express()
+let express = require('express'),
+    app = express(),
+    fs = require('fs')
 
-app.use(express.static('public'))
+
+//Use the dist folder to serve public static files
+app.use(express.static('./public'));
+
+/*
+//setup routes
+app.use(require('./controllers/index.js'))
+
+app.get('/', function (req, res) {
+  res.sendFile('assets/index.html');
+})
+*/
+app.get('/resume', (req, res) =>{
+    res.sendFile(__dirname +'/public/resume_franchipereira.pdf')
+})
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
